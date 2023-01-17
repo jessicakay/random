9031b5754e47873261a05a7163bfd001d0b18409
jessicakay <52629990+jessicakay@users.noreply.github.com>
2023-01-17 09:44:47 -0500

    beep boop

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # misc
 
-This is entirely for my benefit; purely for old code I don't want to lose.
-
-To watch the Snow2 viz, visit: https://www.openprocessing.org/sketch/179672
+This is mostly for old code I don't want to lose.
--- news_tracker.R
+++ news_tracker.R
@@ -6,13 +6,15 @@
 library(ggplot2)
 library(dplyr)
 library(png)
-
+library(stringr)
+library(lubridate)
 
 # clean OAuth tokens and authenticate
-
-detach(package:googlesheets4)
-library(googlesheets4)
+# detach(package:googlesheets4)
 # googlesheets4::gs4_auth()
+ 
+ 
+ library(googlesheets4)
 
 # set global variables
 
@@ -27,14 +29,8 @@ head(dat)
 getthatbread<-function(){
   read_sheet(targsheet) -> dat
   str_extract(dat$EntryPublished,pattern = "[a-zA-Z]+\\s[0-9]+\\,\\s20[0-9]+") -> dat$the_day
+
   dat %>%
-    mutate(article_type = 
-             case_when(
-                grepl(("sport|athletic|athlete|competition"      ),  dat$EntryContent) == TRUE ~"sport",
-                grepl(("prison|prisoner|inmate"                  ),  dat$EntryContent) == TRUE ~"prison",
-                grepl(("school|pupil|principle|teacher|teach"    ),  dat$EntryContent) == TRUE ~"school"
-                )
-           ) %>%
     rename(timestamp = EntryPublished) %>%
     mutate(the_day=as.Date(mdy(dat$the_day))) %>%
     group_by(the_day,region) %>%
@@ -49,13 +45,55 @@ getthatbread<-function(){
              caption=paste("updated",Sys.time()))+
         xlab(element_blank())+
         ylab("number of articles")+
-              theme_classic()+
-        theme(legend.position = "bottom") 
+              theme_bw()+
+        theme(legend.position = "bottom")+
+      facet_grid(region~.)
     } 
 
 getthatbread()
 
 
+# adding stratification by keyword
+
+  read_sheet(targsheet) -> dat
+  str_extract(dat$EntryPublished,pattern = "[a-zA-Z]+\\s[0-9]+\\,\\s20[0-9]+") -> dat$the_day
+  dat %>%
+    rename(timestamp = EntryPublished) %>%
+    mutate(the_day=as.Date(mdy(dat$the_day))) %>%
+    group_by(the_day,region,keyword) %>%
+    mutate(ct=n()) ->> d_set_kw
+  
+    
+  d_set %>%
+    ggplot()+
+    geom_line(aes(x=the_day,y=ct,color=region, colour="daily")) +
+    geom_point(aes(x=the_day,y=ct,color=region, colour="daily")) +
+    labs(title = "Articles about trans people in US + UK news media",
+         subtitle = "https://tech.lgbt/@jessdkant",
+         caption=paste("updated",Sys.time()))+
+    xlab(element_blank())+
+    ylab("number of articles")+
+    theme_classic()+
+    theme(legend.position = "bottom")+
+    facet_grid(.~keyword)
 
 
-d_set %>% select(region,article_type,EntryContent) %>% View()
+
+
+# experimental NLP section, keywords used to further tag items
+
+dat %>% mutate(topic=case_when(
+  str_detect(EntryContent,"(?i)sport|(?i)athlet") == TRUE ~ ("sports"),
+  str_detect(EntryContent,"(?i)school|(?i)educat|(?i)universit") == TRUE ~ list("education"),
+  str_detect(EntryContent,"(?i)restroom|(?i)bathroom|(?i)locker")== TRUE ~ list("bathrooms"),
+  str_detect(EntryContent,"(?i)legislat|(?i)bill") == TRUE ~ list("legislation"))) %>%
+  select(EntryContent,topic) %>% View()
+
+
+dat %>%
+mutate(article_type = 
+         case_when(
+           grepl(("sport|athletic|athlete|competition"      ),  dat$EntryContent) == TRUE ~"sport",
+           grepl(("prison|prisoner|inmate"                  ),  dat$EntryContent) == TRUE ~"prison",
+           grepl(("school|pupil|principle|teacher|teach"    ),  dat$EntryContent) == TRUE ~"school"
+         )) %>% View()
